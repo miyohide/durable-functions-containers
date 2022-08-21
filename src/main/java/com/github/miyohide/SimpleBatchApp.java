254b7c1ed6c8a59a6984f2ee6eb917e29ff6c19a
Hidenori Miyoshi <miyohide@gmail.com>
2022-08-21 14:19:13 +0900

    create sample durable function

---
+++ src/main/java/com/github/miyohide/SimpleBatchApp.java
@@ -0,0 +1,45 @@
+package com.github.miyohide;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.microsoft.durabletask.DurableTaskClient;
+import com.microsoft.durabletask.OrchestrationRunner;
+import com.microsoft.durabletask.azurefunctions.DurableActivityTrigger;
+import com.microsoft.durabletask.azurefunctions.DurableClientContext;
+import com.microsoft.durabletask.azurefunctions.DurableClientInput;
+import com.microsoft.durabletask.azurefunctions.DurableOrchestrationTrigger;
+
+import java.util.Optional;
+
+public class SimpleBatchApp {
+    @FunctionName("StartHelloCities")
+    public HttpResponseMessage startHelloCities(
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET}) HttpRequestMessage<Optional<String>> req,
+            @DurableClientInput(name = "durableContext") DurableClientContext durableClientContext,
+            final ExecutionContext context
+            ) {
+        DurableTaskClient client = durableClientContext.getClient();
+        String instanceId = client.scheduleNewOrchestrationInstance("HelloCities");
+        return durableClientContext.createCheckStatusResponse(req, instanceId);
+    }
+
+    @FunctionName("HelloCities")
+    public String helloCitiesOrchestrator(@DurableOrchestrationTrigger(name = "runtimeState") String runtimeState) {
+        return OrchestrationRunner.loadAndRun(runtimeState, ctx -> {
+            String result = "";
+            result += ctx.callActivity("SayHello", "Tokyo", String.class);
+            result += ctx.callActivity("SayHello", "London", String.class);
+            result += ctx.callActivity("SayHello", "Seattle", String.class);
+            return result;
+        });
+    }
+
+    @FunctionName("SayHello")
+    public String sayHello(@DurableActivityTrigger(name = "name") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
